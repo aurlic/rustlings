5bf915300d67c82099307e91a714d4800e703e16
Arthur Urlic <aurlic@student.42.fr>
2024-11-08 16:04:14 +0100

    feat: vector exercises

--- exercises/05_vecs/vecs1.rs
+++ exercises/05_vecs/vecs1.rs
@@ -3,7 +3,13 @@ fn array_and_vec() -> ([i32; 4], Vec<i32>) {
 
     // TODO: Create a vector called `v` which contains the exact same elements as in the array `a`.
     // Use the vector macro.
-    // let v = ???;
+    let v = vec![a[0], a[1], a[2], a[3]];
+ 
+    // TO UPDATE
+    // v.push(a[0]);
+    // v.push(a[1]);
+    // v.push(a[2]);
+    // v.push(a[3]);
 
     (a, v)
 }
